82d212c29801ea36e9ac4f2240982217d599b10c
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-01 15:14:04 -0600

    Editing

--- db/migrate/20150129210629_create_messages.rb
+++ db/migrate/20150129210629_create_messages.rb
@@ -3,6 +3,8 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.string :message
       # add_column(:messages, :created_at, :datetime)
+        add_column(:message, :created_at, :datetime)
+      add_column(:message, :updated_at, :datetime)
 
        t.timestamps null: false
     end
